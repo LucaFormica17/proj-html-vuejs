fd3a56dc51cfbc7301a90e771872c7b47f1651a3
DarioLoForti <lofortidario@gmail.com>
2024-01-17 19:40:51 +0100

    add Section Most Viewed

---
+++ src/store.js
@@ -0,0 +1,22 @@
+import{ reactive } from 'vue';
+export const store = reactive({
+
+    MostViewed:[
+        {
+            img: 'https://maxcoach.thememove.com/main/wp-content/uploads/sites/1/2020/12/home-personal-finance-blog-03-480x352.jpg',
+            work: "BUSINESS",
+            articol: 'Ten Benefits Of Rentals That May Change Your Perspective',
+            data: 'Dec 28, 2020',
+            views: '3,011 views',
+        },
+        {
+            img: 'https://maxcoach.thememove.com/main/wp-content/uploads/sites/1/2020/12/home-personal-finance-blog-02-480x352.jpg',
+            work: "BUSINESS",
+            articol: '10 Things Successful Mompreneurs Do Different',
+            data: 'Dec 28, 2020',
+            views: '2.643 views',
+        },
+    ]
+   
+})
+
