6183302e5b53f9b834a8fe3532eeb4994ebb7a75
DarioLoForti <lofortidario@gmail.com>
2024-01-17 17:48:01 +0100

    add Jumbotron

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,11 +1,21 @@
 <script>
+import { store } from '../store.js'
+import Jumbotron from './Jumbotron.vue';
 export default {
     name: 'AppMain',
+    components:{
+        Jumbotron,
+    },
+    data() {
+        return {
+            store
+        }
+    },
 }
 </script>
 <template lang="">
     <main>
-        
+        <Jumbotron />
     </main>
 </template>
 <style lang="">
