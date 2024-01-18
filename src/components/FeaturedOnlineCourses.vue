a62a82df5f8f1d1d29e0b4465814e05c4be3568a
Zariy7 <nazariy.hereha@gmail.com>
2024-01-18 09:57:56 +0100

    used prop for course cards, fixed image #1

--- src/components/FeaturedOnlineCourses.vue
+++ src/components/FeaturedOnlineCourses.vue
@@ -1,8 +1,12 @@
 <script>
     import { store } from '../store';
+    import CourseCard from './CourseCard.vue';
 
     export default {
         name: 'FeaturedOnlineCourses',
+        components:{
+            CourseCard,
+        },
         data() {
             return {
                 store
@@ -22,25 +26,7 @@
         </h1>
 
         <div class="row">
-            <div v-for="course, index in store.featuredCourses" :key="index" class="col-6 d-flex my-4">
-                <div class="image-container">
-                    <img :src="`/src/images/${course.img}`" class="round-image">
-                </div>
-                <div class="data-container">
-                    <div class="price">
-                        {{ course.price }}<span v-if="course.priceCents != ''" class="price-cents">{{ course.priceCents }}</span>
-                    </div>
-                    <div class="title">
-                        {{ course.title }} 
-                    </div>
-                    <span class="sub-data">
-                        <i class="far fa-file-alt" /> {{ course.lessons }} Lessons 
-                    </span>
-                    <span class="sub-data">
-                        <i class="far fa-user" /> {{ course.students }} Students 
-                    </span>
-                </div>
-            </div>
+            <CourseCard v-for="course, index in store.featuredCourses" :key="index" :course="course"/>
         </div>
 
         <div class="d-flex justify-content-center">
@@ -73,42 +59,6 @@
             color: white;
             padding: 1rem 3rem;
         }
-    }
-}
-
-.image-container{
-    height: 175px;
-    width: 175px;
-    aspect-ratio: 1 / 1;
-    .round-image{
-        height: 100%;
-        width: 100%;
-        object-fit: cover;
-        border-radius: 50%;
-    }
-}
-
-.data-container{
-    padding: 2rem;
-    .price{
-        font-size: 20px;
-        font-weight: bold;
-        color: $main_green;
-        
-        .price-cents{
-            font-size: 15px;
-        }
-    }
-
-    .title{
-        font-size: 20px;
-        font-weight: bold;
-        color: $main_violet;
-    }
-
-    .sub-data{
-        font-size: 15px;
-        color: $dark_text;
     }
 }
 </style>
