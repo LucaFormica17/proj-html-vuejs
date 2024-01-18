7186608c411200f73cc6cf8308cd54a0b6ef683c
LucaFormica17 <145986784+LucaFormica17@users.noreply.github.com>
2024-01-18 09:59:09 +0100

    incompleted branch

---
+++ src/components/AppSubscribe.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    
+}
+</script>
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+<style lang="scss" scoped>
+@use './styles/generals.scss';
+@use './styles/partials/variables' as*;
+
+
+    
+</style>
